970cf8194aafb30e32a2f2bcf44aae79a2a3f1c1
Patrick <patrickvieiramota@gmail.com>
2021-03-03 17:59:54 -0300

    Create home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { FaPen, FaTrash } from "react-icons/fa";
 
 import Header from '../../components/Header';
 
+import {HomeContainer, Cards} from './styles';
+
+import api from '../../services/api'
+
 function Home(){
+    const [data, setData] = useState({})
+    const [navers, setNavers] = useState([])
+
+    useEffect(()=>{
+        async function loadDatas(){
+            const response = await api.get('e6afe4028c2a1e56e577/users/index')
+
+            setData(response.data)
+            setNavers(response.data.item[0].item)
+        }
+
+        loadDatas()
+    }, [])
+
     return(
         <>
             <Header/>
+            <HomeContainer>
+                <header>
+                    <h1>Navers</h1>
+                    <button>Adicionar Naver</button>
+                </header>
+
+                <Cards>
+                    <li>
+                        <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="Naver"/>
+                        <p>Juliano Reis</p>
+                        <span>Frontend Developer</span>
+                        <div>
+                            <span className="remove"><FaTrash/></span>
+                            <span className="edit"><FaPen/></span>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="Naver"/>
+                        <p>Juliano Reis</p>
+                        <span>Frontend Developer</span>
+                        <div>
+                            <span className="remove"><FaTrash/></span>
+                            <span className="edit"><FaPen/></span>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="Naver"/>
+                        <p>Juliano Reis</p>
+                        <span>Frontend Developer</span>
+                        <div>
+                            <span className="remove"><FaTrash/></span>
+                            <span className="edit"><FaPen/></span>
+                        </div>
+                    </li>
+                    <li>
+                        <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="Naver"/>
+                        <p>Juliano Reis</p>
+                        <span>Frontend Developer</span>
+                        <div>
+                            <span className="remove"><FaTrash/></span>
+                            <span className="edit"><FaPen/></span>
+                        </div>
+                    </li>
+                </Cards>
+
+            </HomeContainer>
         </>
     )
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    padding: 24px 32px;
+    header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    header h1{
+        font-style: normal;
+        font-weight: 600;
+        font-size: 40px;
+        line-height: 48px;
+    }
+
+    header button{
+        font-size: 14px;
+        background: var(--primary);
+        font-weight: 600;
+        color: #FFFFFF;
+        padding: 10px 35px;
+        border: 0;
+        line-height: 24px;
+    }
+
+    @media(max-width: 500px){
+        header h1{
+            font-size: 30px;
+        }
+
+        header button{
+            font-size: 12px;
+            padding: 8px 15px;
+        }
+    }
+`;
+
+export const Cards = styled.div`
+    margin-top: 50px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 10px;
+    row-gap: 30px;
+
+    li{
+        list-style: none;
+    }
+
+    li img{
+        background: var(--secondary);
+        background-size: cover;
+        width: 300px;
+        height: 300px;
+        object-fit: cover;
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
+        filter: gray; 
+    }
+
+    li p{
+        margin: 8px 0 4px;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 18px;
+        color: var(--primary);
+    }
+
+    li > span{
+        color: var(--primary);
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 24px;
+    }
+
+    li > div{
+        margin-top: 8px;
+    }
+
+    li > div > span + span{
+        margin-left: 15px;
+    }
+
+    li > div > .remove:hover::after{
+        content: "Excluir";
+        position: absolute;
+        transform: translate(-100%, -100%);
+        color: #ffffff;
+        font-weight: normal;
+        background: var(--primary);
+        padding: 5px 8px;
+    }
+
+    li > div > .edit:hover::after{
+        content: "Editar";
+        position: absolute;
+        transform: translate(-100%, -100%);
+        color: #ffffff;
+        font-weight: normal;
+        background: var(--primary);
+        padding: 5px 8px;
+    }
+
+    @media(max-width: 1000px){
+        justify-content: space-around;
+    }
+`
